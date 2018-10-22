f9aba6cd1f4f7b17a2c5ce7fdcdb0e36df73ea09
Rhyan Vargas <rhyan@rhyanvargas.com>
2018-10-22 17:41:16 -0400

    seperate StorePicker into its own component file.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';  // ES6 modules
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+
+render(<StorePicker />, document.querySelector('#main'));
