a758c079f925aeb6c05eb427afcd8c5dd231eb04
Rhyan Vargas <rhyan@rhyanvargas.com>
2018-10-22 18:06:46 -0400

    create app js file and header component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello Store Picker!</p>
+        return  (
+            <React.Fragment> 
+            { /* Fragment Allows sibling HTML elements */ }
+            <form action="" className="store-selector">
+                    <h2>Please Enter A Store!</h2> 
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type="submit"> Visit Store >> </button>
+                </form>
+            </React.Fragment>
+        ) 
     }
 }
 
