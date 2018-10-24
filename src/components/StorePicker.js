ff6ba18da5db041e8befe4e9d5b0b1a919613a01
Rhyan Vargas <rhyan@rhyanvargas.com>
2018-10-24 14:59:32 -0400

    redirect to store page when user types in sotre name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,32 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    // Properties
+    myInput = React.createRef();
+    // Making methods into properties allow access to this method using "this"
+    goToStore = (event) => {
+        // 1. Stop Form from submitting
+        event.preventDefault(); 
+        // 2. Get the text from that input
+        const storeName = this.myInput.value.value;
+        console.log(storeName);
+        // 3. Change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`);
+        
+    }
+    
+    
     render() {
         return  (
             <React.Fragment> 
             { /* Fragment Allows sibling HTML elements */ }
-            <form action="" className="store-selector">
+            <form onSubmit={this.goToStore} className="store-selector">
                     <h2>Please Enter A Store!</h2> 
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                    <input 
+                        type="text" 
+                        ref={this.myInput}
+                        required placeholder="Store Name" 
+                        defaultValue={getFunName()} />
                     <button type="submit"> Visit Store >> </button>
                 </form>
             </React.Fragment>
